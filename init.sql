a6cb2a283d616633194ba9cdf8cd0cd7a34951a8
CJacobe92 <jacobe.christian92@gmail.com>
2025-06-11 00:10:01 -0700

    updated the boilerplate code

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE jobscout_db_dev;
+CREATE DATABASE jobscout_db_test;
+CREATE DATABASE jobscout_db_prod;
+
+ALTER DATABASE jobscout_db_dev OWNER TO cjadmin;
+ALTER DATABASE jobscout_db_test OWNER TO cjadmin;
+ALTER DATABASE jobscout_db_prod OWNER TO cjadmin;
